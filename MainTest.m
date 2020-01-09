33577e1c41dd00b92995d5cb6ec8fdc9aa438b38
collado <acolladorosell1992@gmail.com>
2020-01-09 15:23:47 -0300

    only to update

--- MainTest.m
+++ MainTest.m
@@ -5,7 +5,7 @@ clear;
 addpath('lib/');
 
 %Receptor%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Receptor.Tsimulacion = 3; %[s]
+Receptor.Tsimulacion = 7; %[s]
 Receptor.tr = 100e-6 ; % tamano de la ventana de recepcion
 Receptor.T = 5e-6 ; % tamano del pulso transmitido 
 Receptor.c = 3e8; % velocidad de la luz en el vacio
@@ -15,8 +15,8 @@ Receptor.T1 = 0.8e-3 ; %[s] para el caso staggered
 Receptor.T2 = 1.2e-3; %[s] para el caso staggered 3T1 = 2T2
 Receptor.fs = 50e6; %frecuencia de muestreo
 Receptor.Fc = 5.6e9 ; % [Hz] frecuencia portadora
-Receptor.M = 28; %Numero de muestras en acimut dentro de los 3dB de la antena
-Receptor.modalidad = "U"; %S de staggered, U de uniforme
+Receptor.M = 64; %Numero de muestras en acimut dentro de los 3dB de la antena
+Receptor.modalidad = "S"; %S de staggered, U de uniforme
 %Antena%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 Antena.x = 0;
 Antena.y = 0;
@@ -34,15 +34,15 @@ Antena.NoiseLevel = 10^-8 ;
 %%%%%%%%Creo los reflectores%%%%%%%%%%%%%%
 rng('default');
 rng(1);
-% Reflectores1 = reflectores(1000,100,[-5000,12000,914], 2000,-18,3); %%Aproximo a una nuve con distribucion gaussiana en la velocidad, para la DEP
-% Reflectores2 = reflectores(2000,100000,[5000,10000,1100], 700,0,0); 
-Reflectores3 = reflectores(2000,1000,[5000,10000,1100], 2000,-20,3); 
-Reflectores4 = reflectores(2000,1000,[0,10000,1100], 1000,12,2);
-Reflectores5 = reflectores(2000,1000,[-5000,10000,1100], 1500,-10,4);
+% Reflectores1 = reflectores(2000,160000,[-5000,10000,1100], 1000,0,0); %%Aproximo a una nuve con distribucion gaussiana en la velocidad, para la DEP
+Reflectores2 = reflectores(2000,100000,[5000,10000,1100], 700,0,0);
+Reflectores3 = reflectores(2000,1000,[5000,10000,1100], 2000,-40,3);
+Reflectores4 = reflectores(2000,1000,[0,10000,1100], 1000,35,2);
+% Reflectores5 = reflectores(2000,1000,[-5000,10000,1100], 1500,-10,4);
 
 % figure; scatter3(Reflectores2.positionX, Reflectores2.positionY, Reflectores2.positionZ);
 %Uno todos los reflectores
- Reflectores = UneReflectores([  Reflectores3, Reflectores4,  Reflectores5  ]); 
+ Reflectores = UneReflectores([Reflectores2, Reflectores3, Reflectores4 ]); 
 % Reflectores = UneReflectores(Reflectores1 ); 
 clear Reflectores1 Reflectores2 Reflectores3 Reflectores4 Reflectores5 ;
 
@@ -73,4 +73,4 @@ elseif Receptor.modalidad == "U"
     save('simulacionU.mat')
 end
 
-
+disp('termino la simulacion');
